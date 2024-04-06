58fd89d8e4119516890448f2e054f32642ed2703
Dylan <minecraftkingdylan11@gmail.com>
2024-04-06 13:46:55 -0400

    Added Python venv

---
+++ main.py
@@ -0,0 +1,6 @@
+from PyQt5 import QtCore, QtWidgets, QtGui
+
+# QtGui Link:       https://www.riverbankcomputing.com/static/Docs/PyQt5/api/qtgui/qtgui-module.html
+# QtCore Link:      https://www.riverbankcomputing.com/static/Docs/PyQt5/api/qtcore/qtcore-module.html
+# QtWidgets Link:   https://www.riverbankcomputing.com/static/Docs/PyQt5/api/qtwidgets/qtwidgets-module.html
+
